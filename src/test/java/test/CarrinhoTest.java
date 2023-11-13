ccc4744fe30ef03927c67e02f93117b448b281b9
João Astolfo <joao.astolfo@lnls.br>
2023-11-13 19:18:12 -0300

    Testes

--- src/test/java/test/CarrinhoTest.java
+++ src/test/java/test/CarrinhoTest.java
@@ -1,7 +1,40 @@
 package test;
 
+import loja.Carrinho;
+import loja.Produto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarrinhoTest {
+
+    private Carrinho carrinho;
+    private Produto produto;
+
+    @BeforeEach
+    public void init() {
+        carrinho = new Carrinho();
+        produto = new Produto(0,"0",0);
+    }
+
+    @Test
+    public void quandoCarrinhoVazio_retornaValido() {
+        assertTrue(carrinho.isVazio());
+    }
+
+    @Test
+    public void quandoUmProduto_retornaValido(){
+        carrinho.adicionar(produto);
+        assertEquals(1, carrinho.getQtde());
+    }
+
+    @Test
+    public void quandoAdicionaProduto_entaoEncontraProduto(){
+        carrinho.adicionar(produto);
+        assertTrue(carrinho.contem(produto));
+    }
+
+
 
 }
--- src/test/java/test/LojaVirtualTest.java
+++ src/test/java/test/LojaVirtualTest.java
@@ -1,7 +1,27 @@
 package test;
 
+import loja.LojaVirtual;
+import loja.Produto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LojaVirtualTest {
+
+    private LojaVirtual lojaVirtual;
+
+    @BeforeEach
+    public void init() {
+        lojaVirtual = new LojaVirtual();
+    }
+
+    @Test
+    public void quandoCriarProduto_entaoValido(){
+        Produto produto = lojaVirtual.criarProduto();
+        Assertions.assertNotNull(produto);
+        assertEquals(1,produto.getCod());
+    }
 
 }
